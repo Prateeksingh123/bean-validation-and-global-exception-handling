bae20bbb0b5dee7b88eafb28a6ab607eafd043fe
Prateek825 <prateeksingh825@gmail.com>
2025-06-30 10:29:53 +0530

    jenkins chagnes

--- course-service/src/main/java/com/prateek/HelloJenkins.java
+++ course-service/src/main/java/com/prateek/HelloJenkins.java
@@ -2,6 +2,6 @@ package com.prateek;
 
 public class HelloJenkins {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins");
+        System.out.println("Hello Jenkins v2");
     }
 }
